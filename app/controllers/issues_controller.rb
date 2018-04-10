8496d476b201b112a0f2f68f5a4dd1744b78cf25
Shantanu Sinha <shantanusinha95@gmail.com>
2018-04-10 07:14:44 +0530

    Isolated Controllers are ready

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,5 +1,27 @@
 class IssuesController < ApplicationController
+	before_action :authenticate_user!
 	def index
-		render json: {'Hello': 'World'}
+		@issues = Issue.all
+	end
+
+	def show
+		@issue = Issue.find(params[:id])
+	end
+
+	def new 
+		@issue = Issue.new
+	end
+
+	def create 
+		@issue.create(issue_params, current_user.id)
+	end
+
+	def destroy
+	end
+
+	private 
+	
+	def issue_params
+		params.require(:issue).permit(:problem_link, :solution_link, :description, :upvotes)
 	end
 end
--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -1,2 +1,27 @@
 class OpinionsController < ApplicationController
+	before_action :authenticate_user!
+	def index
+		redirect_to root_path
+	end
+
+	def show
+		@opinion = Opinion.find(params[:id])
+	end
+
+	def new 
+		@opinion = Opinion.new
+	end
+
+	def create 
+		Opinion.create(opinion_params, user_id: current_user.id)
+	end
+
+	def destroy
+	end
+
+	private 
+	
+	def opinion_params
+		params.require(:opinion).permit(:solution_approach, :solution_link, :suggestion, :upvotes)
+	end
 end
